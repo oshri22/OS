6493a70728d027e60f3c70d91047c9ce21222a1b
oshri22 <oshritaub@gmail.com>
2021-02-16 10:29:33 +0200

    Add a use of vga buffer . Add global interface for it. Add support for rust print/ln macros.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
-#![no_std]
-#![no_main]
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic (_info: &PanicInfo) -> !{
-    loop{}
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    println!("Hello world!");
+    print!("another test");
+    panic!("panic panic");
+    loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> !{
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}",info);
     loop {}
 }
