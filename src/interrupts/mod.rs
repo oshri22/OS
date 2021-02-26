0c6b786ed487ab245adf95562140938d619f5524
oshri22 <oshritaub@gmail.com>
2021-02-26 19:53:44 +0200

    add a handler macro for exceptions with error code

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -27,17 +27,34 @@ macro_rules! handler {
     }}
 }
 
+macro_rules! handler_with_error_code {
+    ($name: ident) => {{
+        #[naked]
+        extern "C" fn wrapper() -> ! {
+            unsafe {
+                asm!(
+                "pop rsi"
+                "mov rdi, rsp",
+                "sub rsp, 8",
+                "call {}",sym $name);
+                ::core::intrinsics::unreachable();
+            }
+        }
+        wrapper
+    }}
+}
+
 lazy_static! {
     static ref IDT: idt::Idt = {
         let mut idt = idt::Idt::new();
         idt.set_handler(0, handler!(divide_by_zero_handler));
         idt.set_handler(6, handler!(invalid_opcode_handler));
+        idt.set_handler(14, handler_with_error_code!(page_fault_handler));
         idt
     };
 }
 
 extern "C" fn divide_by_zero_handler(stack_frame: &ExceptionStackFrame) -> ! {
-    let stack_frame = unsafe { &*stack_frame };
     println!(
         "\nException: divide by zero at {:#x}\n{:#?}",
         stack_frame.instruction_pointer, stack_frame
@@ -46,10 +63,18 @@ extern "C" fn divide_by_zero_handler(stack_frame: &ExceptionStackFrame) -> ! {
 }
 
 extern "C" fn invalid_opcode_handler(stack_frame: &ExceptionStackFrame) -> ! {
-    let stack_frame = unsafe { &*stack_frame };
     println!(
-        "\nException: invalid opcode at {:#x}\n{:#?}",
-            stack_frame.instruction_pointer, stack_frame
+        "\nException: invalid opcode at {:#x}\n",
+        stack_frame.instruction_pointer
+    );
+
+    loop {}
+}
+
+extern "C" fn page_fault_handler(stack_frame: &ExceptionStackFrame, error_code: u64) -> ! {
+    println!(
+        "\nException: page fault with error code {} at {:#x}",
+        error_code, stack_frame.instruction_pointer
     );
 
     loop {}
