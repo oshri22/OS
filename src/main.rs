b6779b3b31a97ee29dd2a87257c53bdafe7aa5f7
oshri22 <oshritaub@gmail.com>
2021-02-26 19:53:16 +0200

    add a function to test invalid_op int

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 #![feature(asm)]
-#![feature(llvm_asm)]
 #![feature(naked_functions)]
 #![feature(core_intrinsics)]
 
@@ -14,18 +13,25 @@ pub extern "C" fn _start() -> ! {
     //println!("Hello world!");
 
     interrupts::init();
-    raise_div_int();
+
+    //raise_invalid_opcode_int();
+
     println!("didn't crush! dependence");
     loop {}
 }
 
-fn raise_div_int() {
+fn raise_div_by_zero_int() {
     unsafe { asm!("mov dx, 0", "div dx") };
+}
+
+fn raise_invalid_opcode_int() {
+    println!("crush here");
+    unsafe { asm!("ud2") };
 }
 
 /// This function is called on panic.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+    println!("assignment {}", info);
     loop {}
 }
